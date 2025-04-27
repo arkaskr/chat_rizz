c8d7099bb287647a4f3a5a00f4b207376ac439dd
Arka Sarkar <arkaskr24@gmail.com>
2025-04-28 03:11:30 +0530

    started

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,43 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (password.lenngth < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: " Email already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Invalid user data" });
+  }
+};
+
+export const login = (req, res) => {
+  res.send("login route");
+};
+
+export const logout = (req, res) => {
+  res.send("logout route");
+};
