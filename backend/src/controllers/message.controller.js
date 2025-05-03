a213df328f7db7f3e0e5acedd5f129ee08d8b515
Arka Sarkar <arkaskr24@gmail.com>
2025-05-04 01:24:26 +0530

    1-bug

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -56,8 +57,12 @@ export const sendMessage = async (req, res) => {
 
     await newMessage.save();
 
-    res.status(201).json(newMessage)
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller", error.message);
     res.status(500).json({ message: "Internal Server Error" });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,9 +11,11 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
 import { Loader, } from "lucide-react";
 import {Toaster} from "react-hot-toast"
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers} = useAuthStore();
+  const {theme}=useThemeStore()
 
   useEffect(() => {
     checkAuth();
@@ -31,7 +33,7 @@ const App = () => {
   
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
